8f047c57ef4e1641285f7290e584192108c3404d
Bertrand <dan.bertrand.hs@gmail.com>
2021-02-11 09:54:11 +0100

    Finished set up Router with pageList + pageDetails

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import PageList from './pages/pageList';
+import PageDetails from './pages/pageDetails';
 
 const App = () => (
-  <App />
+  <>
+    <h1>App</h1>
+    <Router>
+      <Switch>
+        <Route path="/" exact>
+          <PageList />
+        </Route>
+        <Route path="/pagedetails/:id">
+          <PageDetails />
+        </Route>
+      </Switch>
+    </Router>
+  </>
 );
 
 ReactDOM.render(<App />, document.getElementById('root'));
---
+++ src/pages/pageList/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const PageList = () => (
+  <div>PageList</div>
+);
+
+export default PageList;
