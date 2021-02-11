f729a0ee1c9098aefec1903284fae507fc95dd00
Bertrand <dan.bertrand.hs@gmail.com>
2021-02-11 15:57:57 +0100

    Add the automatic display of future games in the home page

--- src/pages/pageList/index.jsx
+++ src/pages/pageList/index.jsx
@@ -7,21 +7,40 @@ import SearchBar from '../../components/searchBar/index';
 const PageList = () => {
   const [input, setInput] = useState();
   const [search, setSearch] = useState((localStorage.getItem('Search') ? JSON.parse(localStorage.getItem('Search')) : ''));
-  const fetchData = async () => {
+  const [didSearch, setDidSearch] = useState(false);
+  const fetchSearch = async () => {
     if (input && input.length > 2) {
       const inputFormat = input.split(' ').join('%20');
       const response = await fetch(`https://api.rawg.io/api/games?search=${inputFormat}&page_size=27`);
       const json = await response.json();
       setSearch(await json);
       localStorage.setItem('Search', JSON.stringify(await search));
+      setDidSearch(true);
     }
   };
 
-  useEffect(() => { fetchData(); }, [input]);
+  const datePlusTenYears = () => {
+    const date = new Date();
+    const year = date.getFullYear() + 10;
+    const dateNoYear = date.toISOString().slice(4, 10);
+    return year.toString().concat(dateNoYear);
+  };
+
+  const fetchFutureGames = async () => {
+    if (!didSearch) {
+      const futureDates = `?dates=${new Date().toISOString().slice(0, 10)},${datePlusTenYears()}`;
+      const response = await fetch(`https://api.rawg.io/api/games${futureDates}&ordering=-rating`);
+      const json = await response.json();
+      setSearch(await json);
+    }
+  };
+
+  useEffect(() => { fetchSearch(); }, [input]);
+  useEffect(() => { fetchFutureGames(); }, [didSearch]);
 
   return (
     <>
-      <SearchBar input={input} setInput={setInput} fetchData={fetchData} />
+      <SearchBar input={input} setInput={setInput} />
       <div className="game-list">
         {search && search.results.map((game) => <DisplayGame key={game.slug} gameSlug={game.slug} />)}
       </div>
